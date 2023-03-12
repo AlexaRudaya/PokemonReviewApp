a846c6e00489f47d494fdacf7e80ce56d7883af6
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-03-12 15:57:21 +0300

    Add project files.

---
+++ PokemonReviewApp/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public sealed class Country
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public sealed class Owner
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Gym { get; set; }
+    }
+}
